c5bf4bcdcd0ded255ba154cf3248aa7122257cd7
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2024-10-01 16:04:18 +0200

    Bee with controller
    Co-Authored-By: Vatsal Jariwala <105443900+vatsal8784@users.noreply.github.com>

--- Assets/BeeController.cs
+++ Assets/BeeController.cs
@@ -1,23 +1,36 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
+using UnityEngine;
+using UnityEngine.UI; // Add this line to include the namespace for FixedJoystick
 
 public class BeeController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed ;
 
-    private Joystick fixedJoystick;
-    private Rigidbody rigidbody;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private FixedJoystick fixedJoystick;
+    private new Rigidbody rigidbody;
+
+    [System.Obsolete]
     private void OnEnable()
     {
-        fixedJoystick = FindObjectOfType<Joystick>();
+        fixedJoystick = FindObjectOfType<FixedJoystick>();
         rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
+
     private void FixedUpdate()
     {
-        fixedJoystick.
+       float xVal = fixedJoystick.Horizontal;
+       float yVal = fixedJoystick.Vertical;
+
+       Vector3 movement = new Vector3(xVal, 0, yVal);
+       rigidbody.linearVelocity = movement * speed;
+
+       if(xVal != 0 || yVal != 0)
+       {
+           transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(xVal, yVal) * Mathf.Rad2Deg, transform.eulerAngles.z);
+       }
     }
+
+
 }
