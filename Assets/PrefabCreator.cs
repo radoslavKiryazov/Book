bf126b94b3e067b88f7c44e57fe976a38ed8d28c
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2024-10-08 19:14:24 +0200

    goofy but working cloud
    Still haven't figured out how to remove the model when the image is not tracked

--- Assets/PrefabCreator.cs
+++ Assets/PrefabCreator.cs
@@ -5,11 +5,13 @@ using UnityEngine.XR.ARFoundation;
 
 public class PrefabCreator : MonoBehaviour
 {
-    [SerializeField] private GameObject beePrefab;
+    [SerializeField] private GameObject cPrefab;
+    [SerializeField] private GameObject bPrefab;
     [SerializeField] private Vector3 prefabOffset;
 
     private ARTrackedImageManager aRTrackedImageManager;
-    private GameObject bee;
+    private GameObject c;
+    private GameObject b;
 
     private void OnEnable()
     {
@@ -21,8 +23,17 @@ public class PrefabCreator : MonoBehaviour
     {
         foreach (var newImage in args.added)
         {
-            bee = Instantiate(beePrefab, newImage.transform);
-            bee.transform.position += prefabOffset;
+            if (newImage.referenceImage.name == "BeeImage")
+            {
+                b = Instantiate(bPrefab, newImage.transform);
+                b.transform.position += prefabOffset;
+            }
+
+            else if (newImage.referenceImage.name == "CloudImage")
+            {
+                c = Instantiate(cPrefab, newImage.transform);
+                c.transform.position += prefabOffset;
+            }
         }
 
         foreach (var updatedImage in args.updated)
@@ -32,7 +43,18 @@ public class PrefabCreator : MonoBehaviour
 
         foreach (var removedImage in args.removed)
         {
-            // Handle removed event
+            if (removedImage.Value.referenceImage.name == "BeeImage" && b != null)
+            {
+                Destroy(b);  // Destroy the bee when its tracked image is removed
+                b = null;
+                bPrefab.SetActive(false);
+            }
+            else if (removedImage.Value.referenceImage.name == "CloudImage" && c != null)
+            {
+                Destroy(c);  // Destroy the cloud when its tracked image is removed  // Destroy the bee when its tracked image is removed
+                c = null;
+                cPrefab.SetActive(false);
+            }
         }
     }
 }
